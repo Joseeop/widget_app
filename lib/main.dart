155eac1bd3f5e9d1dbc11dc4a464d1efc220d8bf
Jose Ona <onapinedajose@gmail.com>
2023-06-25 17:48:33 +0200

    Home screen implementado

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
-      theme:AppTheme(selectedColor: 3).getTheme(),
+      theme:AppTheme(selectedColor: 0).getTheme(),
       debugShowCheckedModeBanner: false,
       home: const HomeScreen(),
     );
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widget_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,50 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
          title: const Text('Flutter + Material3'),
       ),
+      body:const _HomeView()
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    
+
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (BuildContext context, int index) {
+        final MenuItem menuItem = appMenuItems.elementAt(index);
+      
+      return _CustomListTile(menuItem: menuItem);
+      
+      
+      });
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+
+ 
+    final colors = Theme.of(context).colorScheme;
+    
+    return ListTile(
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      leading: Icon(menuItem.icon, color:colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios_rounded,color: colors.primary,),
+      onTap: (){},
     );
   }
 }
