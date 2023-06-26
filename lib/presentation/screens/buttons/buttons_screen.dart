efd160f2f68154d2fd1f83ccd94bb58f511ec637
Jose Ona <onapinedajose@gmail.com>
2023-06-26 10:15:11 +0200

    Agregados distintos estilos de botones

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:widget_app/config/theme/app_theme.dart';
 
 class ButtosScreens extends StatelessWidget {
   static const String name = 'buttons_name';
@@ -10,7 +12,98 @@ class ButtosScreens extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Buttons Screen'),
       ),
-      body: const Placeholder(),
+      body: const _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_back_ios_new_rounded),
+        onPressed: () {
+          context.pop();
+        },
+      ),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return SizedBox(
+      width:double.infinity ,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 10,
+          vertical: 20
+        ),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+
+            ElevatedButton(
+              onPressed: (){}, 
+              child: const Text('Elevated ')),
+
+            const ElevatedButton(onPressed: null, 
+            child:  Text('Elevated Disable')),
+
+            ElevatedButton.icon(
+              onPressed: (){}, 
+              icon: const Icon(Icons.access_alarm_rounded), 
+              label: const Text('Elevated Icon')
+              ),
+
+            FilledButton(
+              onPressed: (){}, 
+              child: const Text('Filled')
+            ),
+
+            FilledButton.icon(
+              onPressed: (){},
+              icon:const  Icon(Icons.accessibility_new),
+              label: const Text('Filled Icon'), 
+              ),
+
+            OutlinedButton(
+              onPressed: (){}, 
+              child: const Text('Outline')
+            ),
+
+            OutlinedButton.icon(
+            onPressed: (){}, 
+            icon: const Icon(Icons.terminal), 
+            label: const Text('Outline Icon')
+            ),
+
+            TextButton(
+              onPressed: (){},
+              child: const Text('Text')
+            ),
+
+            TextButton.icon(
+              onPressed: (){}, 
+              icon: const Icon(Icons.account_box_outlined),
+              label: const Text('Text Icon')
+            ), 
+
+            IconButton(
+              onPressed: (){}, 
+              icon: const Icon(Icons.app_registration_rounded)
+              ),
+
+             IconButton(
+              onPressed: (){}, 
+              icon: const Icon(Icons.app_registration_rounded),
+              style: ButtonStyle(
+                backgroundColor:MaterialStatePropertyAll(colors.primary),
+                iconColor:const MaterialStatePropertyAll(Colors.white) 
+              ),
+              ),
+
+          ],
+        ),
+      ),
     );
   }
 }
