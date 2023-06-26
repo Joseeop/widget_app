d3b106735842b30b0bab15ca303b46b984a65e5d
Jose Ona <onapinedajose@gmail.com>
2023-06-26 11:54:02 +0200

    screens creadas y barril actualizado

---
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class InfiniteScroll extends StatelessWidget {
+
+  static const name ='infinite_screen';
+  const InfiniteScroll({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,7 +1,15 @@
 
 
 
+export 'package:widget_app/presentation/screens/animated/animated_screen.dart';
+export 'package:widget_app/presentation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widget_app/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widget_app/presentation/screens/cards/cards_screens.dart';
 export 'package:widget_app/presentation/screens/home/home_screen.dart';
+export 'package:widget_app/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widget_app/presentation/screens/progress/progress_screen.dart';
+export 'package:widget_app/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widget_app/presentation/screens/ui_controls/ui_controls_screen.dart';
+
+
 
